4f788874360adc407ec9a68ebc8588599c5eff73
Guilherme Rodz <guilherme.rodrigues.profissional@gmail.com>
2020-09-29 10:55:57 -0300

    Scroll values with useWrapperScroll

--- src/components/Model/useWrapperScroll.ts
+++ src/components/Model/useWrapperScroll.ts
@@ -1,8 +1,42 @@
+import { useContext, useEffect } from 'react'
 import { useMotionValue } from 'framer-motion'
 
+import ModelsContext from './ModelsContext'
+
 export default function useWrapperScroll() {
+  const { wrapperRef } = useContext(ModelsContext)
+
   const scrollY = useMotionValue(0)
   const scrollYProgress = useMotionValue(0)
+
+  useEffect(() => {
+    scrollY.onChange(scrollY => {
+      console.log({ scrollY })
+    })
+    scrollYProgress.onChange(scrollYProgress => {
+      console.log({ scrollYProgress })
+    })
+  }, [scrollY, scrollYProgress])
+
+  useEffect(() => {
+    if (wrapperRef.current) {
+      const updateScrollValue = () => {
+        if (wrapperRef.current) {
+          const { scrollTop, scrollHeight, offsetHeight } = wrapperRef.current
+
+          const fullScroll = scrollHeight - offsetHeight
+
+          scrollY.set(scrollTop)
+          scrollYProgress.set(scrollTop / fullScroll)
+        }
+      }
+
+      wrapperRef.current.addEventListener('scroll', updateScrollValue)
+
+      return () =>
+        wrapperRef?.current?.removeEventListener('scroll', updateScrollValue)
+    }
+  }, [wrapperRef, scrollY, scrollYProgress])
 
   return { scrollY, scrollYProgress }
 }
