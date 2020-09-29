cf706551f16068740a92f0394e72d998c369dadf
Guilherme Rodz <guilherme.rodrigues.profissional@gmail.com>
2020-09-29 09:41:53 -0300

    Register, Unregister and Get Model By Name

--- src/components/Model/ModelsWrapper/index.tsx
+++ src/components/Model/ModelsWrapper/index.tsx
@@ -1,9 +1,30 @@
-import React, { useRef } from 'react'
+import React, { useCallback, useRef, useState } from 'react'
+
+import { CarModel } from '../ModelsContext'
 
 import { Container } from './styles'
 
 const ModelsWrapper: React.FC = ({ children }) => {
   const wrapperRef = useRef<HTMLDivElement>(null)
+
+  const [registeredModels, setRegisteredModels] = useState<CarModel[]>([])
+
+  const registerModel = useCallback((model: CarModel) => {
+    setRegisteredModels(state => [...state, { ...model }])
+  }, [])
+
+  const unregisterModel = useCallback((modelName: string) => {
+    setRegisteredModels(state =>
+      state.filter(model => model.modelName !== modelName)
+    )
+  }, [])
+
+  const getModelByName = useCallback(
+    (modelName: string) => {
+      return registeredModels.find(item => item.modelName === modelName) || null
+    },
+    [registeredModels]
+  )
 
   return <Container ref={wrapperRef}>{children}</Container>
 }
