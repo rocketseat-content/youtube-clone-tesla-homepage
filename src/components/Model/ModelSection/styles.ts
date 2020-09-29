6f0f7ce1bfd14ca2623de308942b9ffe81cfefd7
Guilherme Rodz <guilherme.rodrigues.profissional@gmail.com>
2020-09-29 09:33:54 -0300

    Scroll Snap

--- src/components/Model/ModelSection/styles.ts
+++ src/components/Model/ModelSection/styles.ts
@@ -2,4 +2,6 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   height: 100vh;
+
+  scroll-snap-align: start;
 `
--- src/components/Model/ModelsWrapper/styles.ts
+++ src/components/Model/ModelsWrapper/styles.ts
@@ -1,3 +1,8 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  height: 100vh;
+
+  scroll-snap-type: y mandatory;
+  overflow-y: scroll;
+`
