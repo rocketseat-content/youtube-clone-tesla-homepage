2f5c035516e677e2611b00ef8a6bea2b1bd6bd38
Guilherme Rodz <guilherme.rodrigues.profissional@gmail.com>
2020-09-29 09:57:35 -0300

    Styled Overlays

--- src/components/Model/ModelsWrapper/styles.ts
+++ src/components/Model/ModelsWrapper/styles.ts
@@ -7,6 +7,21 @@ export const Container = styled.div`
   overflow-y: scroll;
 `
 
-export const OverlaysRoot = styled.div``
+export const OverlaysRoot = styled.div`
+  position: sticky;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+`
 
-export const ModelOverlay = styled.div``
+export const ModelOverlay = styled.div`
+  position: sticky;
+  top: 0;
+  height: 100vh;
+  margin-top: -100vh;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
